8ebff5b831f5a8e8bcc4e976b8b3c0a6324aafbd
UppiliPavitra <pavitrauppili@gmail.com>
2026-02-17 15:43:31 +0530

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,4 +1,8 @@
 # Team Name: Dream Coders
-def calculate_total_bill(amount:float,tip_percent:int)->float:
-    total=float(amount)+(float(amount)*(tip_percent/100))
-    return round(total,2)
+def calculate_total_bill(amount: float, tip_percent: int) -> float:
+    total = amount + (amount * tip_percent / 100)
+    return round(total, 2)
+amount = float(input())
+tip_percent = int(input())
+result = calculate_total_bill(amount, tip_percent)
+print(result)
